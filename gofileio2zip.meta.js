99c892b610ef359e1450db2199d4b2dc2daa740c
Adhya Adam <adhyaadam@gmail.com>
2021-05-23 05:55:33 +0800

    Fix value alignment

--- gofileio2zip.meta.js
+++ gofileio2zip.meta.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  Plugin for a functional 'Create Zip' feature on Gofile.io
 // @author       Adhya Adam Sulthan
-// @updateURL	   https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.meta.js
+// @updateURL    https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.meta.js
 // @downloadURL	 https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.js
 // @match        https://gofile.io/d/*
 // @icon         https://raw.githubusercontent.com/soukied/gofile.io-to-zip/main/gofileio-icon.png
