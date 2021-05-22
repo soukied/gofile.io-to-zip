69bb132fa96114ff1f352e6fa3d8711934e8a904
Adhya Adam <adhyaadam@gmail.com>
2021-05-23 05:50:51 +0800

    Change @updateURL and @downloadURL value

--- gofileio2zip.meta.js
+++ gofileio2zip.meta.js
@@ -4,8 +4,8 @@
 // @version      0.2
 // @description  Plugin for a functional 'Create Zip' feature on Gofile.io
 // @author       Adhya Adam Sulthan
-// @updateURL	 https://rawcdn.githack.com/soukied/gofile.io-to-zip/ad08ac8a229b2d9d30fb22ec59e68402ed38f0d6/gofileio2zip.meta.js
-// @downloadURL	 https://rawcdn.githack.com/soukied/gofile.io-to-zip/ad08ac8a229b2d9d30fb22ec59e68402ed38f0d6/gofileio2zip.js
+// @updateURL	   https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.meta.js
+// @downloadURL	 https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.js
 // @match        https://gofile.io/d/*
 // @icon         https://raw.githubusercontent.com/soukied/gofile.io-to-zip/main/gofileio-icon.png
 // @grant        none
