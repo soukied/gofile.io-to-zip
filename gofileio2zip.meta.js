ad08ac8a229b2d9d30fb22ec59e68402ed38f0d6
Adhya Adam <adhyaadam@gmail.com>
2021-05-23 05:35:12 +0800

    Create gofileio2zip.meta.js

---
+++ gofileio2zip.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Gofile.io to Zip
+// @namespace    https://github.com/soukied/gofile.io-to-zip
+// @version      0.2
+// @description  Plugin for a functional 'Create Zip' feature on Gofile.io
+// @author       Adhya Adam Sulthan
+// @match        https://gofile.io/d/*
+// @icon         https://raw.githubusercontent.com/soukied/gofile.io-to-zip/main/gofileio-icon.png
+// @grant        none
+// ==/UserScript==
