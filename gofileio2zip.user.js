298ffdc86903d83cc1f6a0611c08f7824da9ebcc
Adhya Adam <adhyaadam@gmail.com>
2021-05-27 00:43:29 +0800

    Update gofileio2zip.user.js

--- gofileio2zip.user.js
+++ gofileio2zip.user.js
@@ -5,7 +5,7 @@
 // @description  Plugin for a functional 'Create Zip' feature on Gofile.io
 // @author       Adhya Adam Sulthan
 // @updateURL	 https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.meta.js
-// @downloadURL	 https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.js
+// @downloadURL	 https://raw.githack.com/soukied/gofile.io-to-zip/main/gofileio2zip.user.js
 // @match        https://gofile.io/d/*
 // @icon         https://raw.githubusercontent.com/soukied/gofile.io-to-zip/main/gofileio-icon.png
 // @grant        none
